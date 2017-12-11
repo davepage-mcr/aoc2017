db93295155cd61becd9adf0cc4ad4607b03c3e6f
Dave Page <dave@davepage.me.uk>
2017-12-11 00:19:53 +0000

    Got this working just about

--- day10/knot2.py
+++ day10/knot2.py
@@ -5,12 +5,7 @@ from itertools import cycle
 
 # Read the input
 
-listlength = int(sys.argv[1])
-inputfile = open(sys.argv[2])
-
-circle = range(0,listlength)
-
-instructions = [ int(x) for x in inputfile.readline().rstrip().split(',') ]
+inputfile = open(sys.argv[1])
 
 def printlist (mylist, current, end):
     for index in range(0,len(mylist)):
@@ -26,38 +21,59 @@ def printlist (mylist, current, end):
 
     print
 
+# Need numbers from 0 to 255
+circle = range(0,256)
+
+# Treat input as ASCII characters
+instructions = [ ord(x) for x in inputfile.readline().rstrip() ]
+
+# Add fixed suffix
+instructions += [17, 31, 73, 47, 23]
+
+
 index = 0
 skipsize = 0
 
-for inst in instructions:
-    print "*** Instruction: reverse sublist of length", inst, "starting from position", index
+# Now follow the instructions 64 times, maintaining current index and skipsize
+for myround in range(0,64):
+    print "*** Round", myround, "; index is", index, "; skipsize is", skipsize
+    # print "Instructions:", instructions
+    for inst in instructions:
+        # Step 1 & 2 - find start, select length to reverse
+        # printlist(circle, index, (index + inst - 1 ) % len(circle))
+        # print "Need to select", inst, "characters from", index, "to", (index + inst - 1) % len(circle)
+    
+        # Step 3 - reverse sublist
+        # Python list slices don't seem to wrap aroudn the end which is irritating
+    
+        sublist = []
+        for i in range(0,inst):
+            sublist.append(circle[(index + i) % len(circle)])
+        # print "Sublist to reverse:", sublist
+        sublist = sublist[::-1]
+        # print "Reversed to:", sublist
+    
+        for i in range(0,inst):
+            circle[(index + i) % len(circle)] = sublist[i]
+    
+        # Step 4 - incrementing
+        index += inst + skipsize
+        index %= len(circle)
+        skipsize += 1
+        skipsize %= len(circle)
 
-    # Step 1 & 2 - find start, select length to reverse
-    printlist(circle, index, (index + inst - 1 ) % len(circle))
-    print "Need to select", inst, "characters from", index, "to", (index + inst - 1) % len(circle)
-
-    # Step 3 - reverse sublist
-
-    # Python list slices don't seem to wrap aroudn the end which is irritating
-
-    sublist = []
-    for i in range(0,inst):
-        sublist.append(circle[(index + i) % len(circle)])
-    print "Sublist to reverse:", sublist
-    sublist = sublist[::-1]
-    print "Reversed to:", sublist
-
-    for i in range(0,inst):
-        circle[(index + i) % len(circle)] = sublist[i]
-
-    # however, the "cycle" iterator from itertools may save us a bit of hassle
-    # https://stackoverflow.com/questions/25470799/circular-list-in-python
- 
-    # Step 4 - incrementing
-    index += inst + skipsize
-    index %= len(circle)
-    skipsize += 1
-
+print "This is our sparse hash, list of numbers from 0 to 255 in some order"
 printlist(circle, -1, -1)
 
-print "First two multiplied is", circle[0] * circle[1]
+# Create Dense Hash - 16 blocks of 16 numbers, xored
+string = ''
+for block in range(0,16):
+    densehash = 0
+    blocknums = circle[block * 16:(block + 1)*16]
+    for num in blocknums:
+        densehash ^= num
+    hexchars = "{0:02x}".format(densehash)
+    print blocknums, densehash, hexchars
+    string += hexchars
+
+print string
